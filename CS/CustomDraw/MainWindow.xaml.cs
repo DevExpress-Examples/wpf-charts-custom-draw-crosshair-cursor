fa4a6f50b1932c2b30adf5b2680d703080a412e8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CustomDraw/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using DevExpress.Xpf.Charts;
+
+namespace CustomDraw
+{
+
+    public partial class MainWindow : Window
+    {
+        bool handleCustomDraw;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            handleCustomDraw = true;
+            chartControl.CustomDrawCrosshair += chartControl_CustomDrawCrosshair;
+        }
+        private void chartControl_CustomDrawCrosshair(object sender, CustomDrawCrosshairEventArgs e)
+        {
+            if (!handleCustomDraw) return;
+
+            // Specify the crosshair argument line color, dash style and thickness.
+            e.CrosshairLineElement.Brush = Brushes.Aqua;
+            e.CrosshairLineElement.LineStyle.DashStyle = DashStyles.DashDot;
+            e.CrosshairLineElement.LineStyle.Thickness = 3;
+
+            // Specify  the background  and font size for the crosshair argument label. 
+            foreach (CrosshairAxisLabelElement axisLabelElement in e.CrosshairAxisLabelElements)
+            {
+                axisLabelElement.Background = Brushes.Blue;
+                axisLabelElement.FontSize = 14;
+            }
+
+            foreach (CrosshairElement element in e.CrosshairElements)
+            {
+                // Specify the color, dash style and thickness for the crosshair value lines.                 
+                element.LineElement.Brush = Brushes.DarkViolet;
+                element.LineElement.LineStyle.DashStyle = DashStyles.Dash;
+                element.LineElement.LineStyle.Thickness = 2;
+
+                // Specify the  font size and background for the crosshair value labels.
+                element.AxisLabelElement.FontSize = 14;
+                element.AxisLabelElement.Background = Brushes.Red;
+
+                // Specify the foreground and  font size for the crosshair  cursor label that shows series. 
+                element.LabelElement.Foreground = Brushes.Green;
+                element.LabelElement.FontSize = 14;
+
+            }
+        }
+    }
+}
---
+++ VB/CustomDraw/MainWindow.xaml.vb
@@ -0,0 +1,53 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports System.Windows.Media
+Imports DevExpress.Xpf.Charts
+
+Namespace CustomDraw
+
+	Partial Public Class MainWindow
+		Inherits Window
+		Private handleCustomDraw As Boolean
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			handleCustomDraw = True
+			AddHandler chartControl.CustomDrawCrosshair, AddressOf chartControl_CustomDrawCrosshair
+		End Sub
+		Private Sub chartControl_CustomDrawCrosshair(ByVal sender As Object, ByVal e As CustomDrawCrosshairEventArgs)
+			If (Not handleCustomDraw) Then
+				Return
+			End If
+
+			' Specify the crosshair argument line color, dash style and thickness.
+			e.CrosshairLineElement.Brush = Brushes.Aqua
+			e.CrosshairLineElement.LineStyle.DashStyle = DashStyles.DashDot
+			e.CrosshairLineElement.LineStyle.Thickness = 3
+
+			' Specify  the background  and font size for the crosshair argument label. 
+			For Each axisLabelElement As CrosshairAxisLabelElement In e.CrosshairAxisLabelElements
+				axisLabelElement.Background = Brushes.Blue
+				axisLabelElement.FontSize = 14
+			Next axisLabelElement
+
+			For Each element As CrosshairElement In e.CrosshairElements
+				' Specify the color, dash style and thickness for the crosshair value lines.                 
+				element.LineElement.Brush = Brushes.DarkViolet
+				element.LineElement.LineStyle.DashStyle = DashStyles.Dash
+				element.LineElement.LineStyle.Thickness = 2
+
+				' Specify the  font size and background for the crosshair value labels.
+				element.AxisLabelElement.FontSize = 14
+				element.AxisLabelElement.Background = Brushes.Red
+
+				' Specify the foreground and  font size for the crosshair  cursor label that shows series. 
+				element.LabelElement.Foreground = Brushes.Green
+				element.LabelElement.FontSize = 14
+
+			Next element
+		End Sub
+	End Class
+End Namespace
